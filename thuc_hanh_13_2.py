a9c40667102a09a73282258785bedc20751e1a0a
Tuan2305 <130633938+Tuan2305@users.noreply.github.com>
2025-02-13 14:49:35 +0700

    bai thuc hanh

---
+++ thuc_hanh_13_2.py
@@ -0,0 +1,81 @@
+# # 1. Nhập vào một số nguyên, kiểm tra xem số đó là số âm, dương hay bằng 0 và in kết quả tương ứng.
+
+# number = int(input("nhap so nguyen: "))
+# if number > 0:
+#     print("so duong")
+# elif number < 0:
+#     print("so am")
+
+# # 2. Nhập vào tuổi của một người, nếu người đó từ 18 tuổi trở lên thì in "Adult", ngược lại in "Minor".
+
+# age = int(input("nhap tuoi "))
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+# # 3. Nhập vào một số thực, chuyển nó thành số nguyên và in kết quả.
+# number = float(input("nhap so thuc: "))
+# print(int(number))
+
+# # 4. Nhập vào một số nguyên từ người dùng, nếu không phải số nguyên thì in ra thông báo lỗi "Please enter a valid number."
+# number = input("nhap so nguyen: ")
+# if number.isdigit():
+#     print(int(number))
+# else:
+#     print("Please enter a valid number.")
+    
+# # 5. Viết hàm kiểm tra xem một số có phải là số nguyên tố hay không.
+
+# number = int(input("nhao 1 so "))
+# def ktra_snt(number):
+#     if number < 2:
+#         return False
+#     for i in range(2, number):
+#         if number % i == 0:
+#             return False
+#     return True
+# if ktra_snt(number):
+#     print("la snt")
+# else:
+#     print("khong la snt")
+    
+# # 6. Khai báo một danh sách và sử dụng hàm len() để in ra số phần tử trong danh sách.
+# a = [1, 2, 3, 4, 5]
+# print(len(a))
+
+# # 7. Khai báo một danh sách số và sử dụng hàm max() để in ra giá trị lớn nhất trong danh sách.
+
+# b = [1,6,2,7,2,9,8]
+# print(max(b))
+
+# # 8. Tạo một danh sách các số, sau đó thêm một số mới vào cuối danh sách và in kết quả.
+# c = [4,6,8,3,2,7]
+# c.append(9)
+# print(c)
+# # 9. Tạo một danh sách chứa tên của 5 người, in ra tên thứ ba trong danh sách.
+# listt = ["pham", "minh", "tuan", "dep", "trai"]
+# print(listt[2])
+
+# # 10. Tạo một dictionary chứa thông tin tên và tuổi của một người, in ra tên và tuổi của người đó.
+# dict = {"name": "tuan", "age": 20}
+# print(dict["name"], dict["age"])
+
+
+# # 11. Tạo một dictionary chứa tên các học sinh và điểm của họ, in ra điểm của một học sinh cụ thể.
+# dictti = {"pham": 10, "minh": 9, "tuan": 8}
+# print(dictti["pham"])
+
+
+# 12. Dùng vòng lặp for để in ra các số từ 1 đến 10.
+# for i in range(1, 11):
+#     print(i)
+
+# 13. Dùng vòng lặp while để in ra các số chẵn từ 0 đến 20.
+# i = 0
+# while i <= 20:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+
+        
